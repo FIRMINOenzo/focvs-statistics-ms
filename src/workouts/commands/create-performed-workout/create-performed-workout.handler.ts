d14a14486cc6e0f3a25f1b8fa9a53791c2f87923
FIRMINOenzo <enzofirmino.camp@gmail.com>
2024-08-17 16:16:48 -0300

    feat: remove secret

--- src/workouts/commands/create-performed-workout/create-performed-workout.handler.ts
+++ src/workouts/commands/create-performed-workout/create-performed-workout.handler.ts
@@ -2,7 +2,12 @@ import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
 import { CreatePerformedWorkoutCommand } from './create-performed-workout.command';
 import { CreatePerformedWorkoutDto } from './create-performed-workout.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { MinPerformedExercise } from 'src/workouts/types/min-performed-exercise.type';
 
+type ExerciseExecutionParams = {
+  weight: number;
+  reps: number;
+};
 @CommandHandler(CreatePerformedWorkoutCommand)
 export class CreatePerformedWorkoutHandler
   implements
@@ -30,5 +35,40 @@ export class CreatePerformedWorkoutHandler
     } catch (error) {
       PrismaService.handleError(error);
     }
+  }
+
+  private calculateIfHasImprovements(
+    exercises: Omit<MinPerformedExercise, 'id'>[]
+  ) {
+    exercises.forEach(async (exercise) => {
+      const lastImprovement = await this.searchForImprovements(exercise);
+
+      if (
+        !lastImprovement ||
+        exercise.weight > lastImprovement.weight ||
+        exercise.reps > lastImprovement.reps
+      ) {
+        exercise.hasImprovements = true;
+      }
+    });
+  }
+
+  private async searchForImprovements(
+    exercise: Omit<MinPerformedExercise, 'id'>
+  ) {
+    const lastImprovement = await this.prismaService.$queryRaw<
+      ExerciseExecutionParams[]
+    >`
+      SELECT e.reps, e.weight 
+      FROM performedExercise e 
+      INNER JOIN performedWorkout w ON e.workoutId = w.id
+      WHERE e.exerciseId = ${exercise.exerciseId} AND e.hasImprovements = true
+      ORDER BY w.date DESC LIMIT 1`;
+
+    if (lastImprovement.length > 0) {
+      return lastImprovement[0];
+    }
+
+    return null;
   }
 }
